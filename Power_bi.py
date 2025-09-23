e925e2a089b44c254a5eba87cdddefcaea46ded0
sdveiga <samueldavid@mvvs.com.br>
2025-09-23 00:17:11 -0300

    Update Power_bi.py

--- Power_bi.py
+++ Power_bi.py
@@ -6,8 +6,7 @@ import os
 
 st.markdown("""
     <style>
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
+    
 
     /* Estiliza a sidebar */
     [data-testid="stSidebar"] {
@@ -220,6 +219,7 @@ else:
     # 📈 Exibe o relatório correspondente no corpo principal
     st.markdown(f"### 📊 Relatório: {selecionado}")
     st.components.v1.iframe(powerbi_links[selecionado], height=600, scrolling=True)
+
 
 
 
