bdbecd437fd5c7c9ac80f9c6e8fcfb22ae7699e4
sdveiga <samueldavid@mvvs.com.br>
2025-09-25 22:00:44 -0300

    Update Power_bi.py

--- Power_bi.py
+++ Power_bi.py
@@ -6,7 +6,8 @@ import os
 
 st.markdown("""
     <style>
-   
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
     </style>
 """, unsafe_allow_html=True)
 
@@ -224,6 +225,7 @@ else:
     if st.sidebar.button("🔒 Sair"):
         st.session_state.logado = False
         st.experimental_rerun()
+
 
 
 
