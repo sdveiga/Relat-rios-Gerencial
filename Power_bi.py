511413d9c3d760f008a66d6b0c8e6a2ae9ed4db8
sdveiga <samueldavid@mvvs.com.br>
2025-09-28 15:03:08 -0300

    Update Power_bi.py

--- Power_bi.py
+++ Power_bi.py
@@ -167,8 +167,7 @@ else:
     # 📈 Exibe relatório Power BI
     if selecionado_label not in ["📋 ICG", "🖥️ Apresentação ICG"]:
         if selecionado and selecionado in powerbi_links:
-            st.markdown(f"### 📊 Relatório: {selecionado_label}")
-            st.markdown(f"[Abrir formulário IVM]({powerbi_links[selecionado]})")
+            st.markdown(f"### 📊 Relatório: {selecionado_label}")            
             st.components.v1.iframe(powerbi_links[selecionado], height=600, scrolling=True)
             
         else:
@@ -235,6 +234,7 @@ else:
         st.experimental_rerun()
 
         
+
 
 
 
