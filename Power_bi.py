e45b9ce2f515e533228e14cc550eead8c82b717e
sdveiga <samueldavid@mvvs.com.br>
2025-09-29 11:01:09 -0300

    Update Power_bi.py

--- Power_bi.py
+++ Power_bi.py
@@ -62,11 +62,11 @@ usuarios = {
     "tecnico_user": {
         "senha": "123", "cargo": "TECNICO", "nome": "Carlos Silva",
         "foto": "padrao.png", "admissao": "15/03/2020", "funcionarios": 0
-    }
+    },
     "sdveiga": {
         "senha": "Samu@3887", "cargo": "GERENTE", "nome": "Samuel David Veiga",
         "foto": "gerente.png", "admissao": "01/07/2008", "funcionarios": 360
-    },    
+    }    
 }
 
 # 🔗 Relatórios Power BI simulados
@@ -237,5 +237,6 @@ else:
         st.experimental_rerun()
 
         
+
 
 
